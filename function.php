7f11fbc4b6221a92011e9c5371fa20ee6a755a4f
shahad abdalmuhsen <66949566+shahdCS@users.noreply.github.com>
2020-06-30 00:20:15 +0300

    Add files via upload

---
+++ function.php
@@ -0,0 +1,24 @@
+<?php
+      
+        if(isset($_POST['button1'])) { 
+            echo "Left"; 
+        } 
+        if(isset($_POST['button2'])) { 
+            echo "Forward"; 
+        } 
+        if(isset($_POST['button3'])) { 
+            echo "Stop"; 
+        } 
+        if(isset($_POST['button4'])) { 
+            echo "Backward"; 
+        } 
+        if(isset($_POST['button5'])) { 
+            echo "Right"; 
+        } 
+    ?> 
+
+<!DOCTYPE html>
+<html>
+<head></head>
+<body style="background-color:#e7e7e7;"></body>
+</html>
